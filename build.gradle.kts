778f13a26e165ae6798f49354fc56bbba18892b7
Gematik <software-development@gematik.de>
2023-11-20 17:16:51 +0100

    v1.0.7

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    //trick: for the same plugin versions in all sub-modules
+    id("com.android.application").version("8.0.0").apply(false)
+    id("com.android.library").version("8.0.0").apply(false)
+    kotlin("android").version("1.8.21").apply(false)
+    kotlin("multiplatform").version("1.8.21").apply(false)
+}
+
+buildscript {
+    repositories {
+        gradlePluginPortal()
+    }
+
+    dependencies {
+        classpath("dev.icerock.moko:resources-generator:0.22.0")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
